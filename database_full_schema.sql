40a34f0ef3ef2aee124b1830465c68c975e2d3b5
Victor Alfredo <VictorAlfred28>
2026-03-12 19:42:38 -0300

    Script de backup de base de datos actualizado con las últimas correcciones de relaciones

--- database_full_schema.sql
+++ database_full_schema.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE organizations (
 	id SERIAL NOT NULL, 
 	name VARCHAR, 
@@ -11,47 +12,10 @@ CREATE TABLE organizations (
 	google_calendar_id VARCHAR, 
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT now(), 
 	PRIMARY KEY (id)
-);
+)
 
-CREATE INDEX ix_organizations_name ON organizations (name);
+;
 
-CREATE INDEX ix_organizations_slug ON organizations (slug);
-
-CREATE INDEX ix_organizations_id ON organizations (id);
-
-CREATE TABLE users (
-	id SERIAL NOT NULL, 
-	username VARCHAR, 
-	password_hash VARCHAR, 
-	org_id INTEGER, 
-	is_admin BOOLEAN, 
-	is_superadmin BOOLEAN, 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(org_id) REFERENCES organizations (id)
-);
-
-CREATE INDEX ix_users_username ON users (username);
-
-CREATE INDEX ix_users_id ON users (id);
-
-CREATE TABLE services (
-	id SERIAL NOT NULL, 
-	org_id INTEGER, 
-	name VARCHAR, 
-	price FLOAT, 
-	description TEXT, 
-	category VARCHAR, 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(org_id) REFERENCES organizations (id)
-);
-
-CREATE INDEX ix_services_org_id ON services (org_id);
-
-CREATE INDEX ix_services_id ON services (id);
-
-CREATE INDEX ix_services_category ON services (category);
-
-CREATE INDEX ix_services_name ON services (name);
 
 CREATE TABLE owners (
 	id SERIAL NOT NULL, 
@@ -62,15 +26,55 @@ CREATE TABLE owners (
 	PRIMARY KEY (id), 
 	CONSTRAINT _org_phone_uc UNIQUE (org_id, phone_number), 
 	FOREIGN KEY(org_id) REFERENCES organizations (id)
-);
+)
 
-CREATE INDEX ix_owners_org_id ON owners (org_id);
+;
 
-CREATE INDEX ix_owners_phone_number ON owners (phone_number);
 
-CREATE INDEX ix_owners_id ON owners (id);
+CREATE TABLE services (
+	id SERIAL NOT NULL, 
+	org_id INTEGER, 
+	name VARCHAR, 
+	price FLOAT, 
+	description TEXT, 
+	category VARCHAR, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(org_id) REFERENCES organizations (id)
+)
 
-CREATE INDEX ix_owners_name ON owners (name);
+;
+
+
+CREATE TABLE users (
+	id SERIAL NOT NULL, 
+	username VARCHAR, 
+	password_hash VARCHAR, 
+	org_id INTEGER, 
+	is_admin BOOLEAN, 
+	is_superadmin BOOLEAN, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(org_id) REFERENCES organizations (id)
+)
+
+;
+
+
+CREATE TABLE appointments (
+	id SERIAL NOT NULL, 
+	org_id INTEGER, 
+	pet_name VARCHAR, 
+	reason VARCHAR, 
+	owner_id INTEGER, 
+	date TIMESTAMP WITH TIME ZONE, 
+	status VARCHAR, 
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT now(), 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(org_id) REFERENCES organizations (id), 
+	FOREIGN KEY(owner_id) REFERENCES owners (id)
+)
+
+;
+
 
 CREATE TABLE patients (
 	id SERIAL NOT NULL, 
@@ -88,15 +92,10 @@ CREATE TABLE patients (
 	CONSTRAINT _org_owner_pet_uc UNIQUE (org_id, owner_id, name), 
 	FOREIGN KEY(org_id) REFERENCES organizations (id), 
 	FOREIGN KEY(owner_id) REFERENCES owners (id)
-);
+)
 
-CREATE INDEX ix_patients_name ON patients (name);
+;
 
-CREATE INDEX ix_patients_owner_id ON patients (owner_id);
-
-CREATE INDEX ix_patients_org_id ON patients (org_id);
-
-CREATE INDEX ix_patients_id ON patients (id);
 
 CREATE TABLE clinical_records (
 	id SERIAL NOT NULL, 
@@ -109,13 +108,45 @@ CREATE TABLE clinical_records (
 	PRIMARY KEY (id), 
 	FOREIGN KEY(org_id) REFERENCES organizations (id), 
 	FOREIGN KEY(patient_id) REFERENCES patients (id)
-);
+)
 
-CREATE INDEX ix_clinical_records_org_id ON clinical_records (org_id);
+;
 
-CREATE INDEX ix_clinical_records_patient_id ON clinical_records (patient_id);
 
-CREATE INDEX ix_clinical_records_id ON clinical_records (id);
+CREATE TABLE medical_attentions (
+	id SERIAL NOT NULL, 
+	org_id INTEGER, 
+	patient_id INTEGER, 
+	vet_id INTEGER, 
+	status VARCHAR, 
+	start_date TIMESTAMP WITH TIME ZONE DEFAULT now(), 
+	end_date TIMESTAMP WITH TIME ZONE, 
+	notes TEXT, 
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT now(), 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(org_id) REFERENCES organizations (id), 
+	FOREIGN KEY(patient_id) REFERENCES patients (id), 
+	FOREIGN KEY(vet_id) REFERENCES users (id)
+)
+
+;
+
+
+CREATE TABLE premium_certificates (
+	id SERIAL NOT NULL, 
+	org_id INTEGER, 
+	patient_id INTEGER, 
+	file_hash VARCHAR, 
+	storage_path VARCHAR, 
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT now(), 
+	is_valid BOOLEAN, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(org_id) REFERENCES organizations (id), 
+	FOREIGN KEY(patient_id) REFERENCES patients (id)
+)
+
+;
+
 
 CREATE TABLE vaccinations (
 	id SERIAL NOT NULL, 
@@ -133,92 +164,10 @@ CREATE TABLE vaccinations (
 	PRIMARY KEY (id), 
 	FOREIGN KEY(org_id) REFERENCES organizations (id), 
 	FOREIGN KEY(patient_id) REFERENCES patients (id)
-);
+)
 
-CREATE INDEX ix_vaccinations_patient_id ON vaccinations (patient_id);
+;
 
-CREATE INDEX ix_vaccinations_org_id ON vaccinations (org_id);
-
-CREATE INDEX ix_vaccinations_next_dose_date ON vaccinations (next_dose_date);
-
-CREATE INDEX ix_vaccinations_id ON vaccinations (id);
-
-CREATE INDEX ix_vaccinations_vaccine_name ON vaccinations (vaccine_name);
-
-CREATE TABLE premium_certificates (
-	id SERIAL NOT NULL, 
-	org_id INTEGER, 
-	patient_id INTEGER, 
-	file_hash VARCHAR, 
-	storage_path VARCHAR, 
-	created_at TIMESTAMP WITH TIME ZONE DEFAULT now(), 
-	is_valid BOOLEAN, 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(org_id) REFERENCES organizations (id), 
-	FOREIGN KEY(patient_id) REFERENCES patients (id)
-);
-
-CREATE INDEX ix_premium_certificates_file_hash ON premium_certificates (file_hash);
-
-CREATE INDEX ix_premium_certificates_patient_id ON premium_certificates (patient_id);
-
-CREATE INDEX ix_premium_certificates_id ON premium_certificates (id);
-
-CREATE INDEX ix_premium_certificates_org_id ON premium_certificates (org_id);
-
-CREATE TABLE appointments (
-	id SERIAL NOT NULL, 
-	org_id INTEGER, 
-	pet_name VARCHAR, 
-	reason VARCHAR, 
-	owner_id INTEGER, 
-	date TIMESTAMP WITH TIME ZONE, 
-	status VARCHAR, 
-	created_at TIMESTAMP WITH TIME ZONE DEFAULT now(), 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(org_id) REFERENCES organizations (id), 
-	FOREIGN KEY(owner_id) REFERENCES owners (id)
-);
-
-CREATE INDEX idx_apps_org_date ON appointments (org_id, date);
-
-CREATE INDEX ix_appointments_id ON appointments (id);
-
-CREATE INDEX ix_appointments_pet_name ON appointments (pet_name);
-
-CREATE INDEX ix_appointments_status ON appointments (status);
-
-CREATE INDEX idx_apps_org_status ON appointments (org_id, status);
-
-CREATE INDEX ix_appointments_org_id ON appointments (org_id);
-
-CREATE INDEX ix_appointments_date ON appointments (date);
-
-CREATE INDEX ix_appointments_owner_id ON appointments (owner_id);
-
-CREATE TABLE medical_attentions (
-	id SERIAL NOT NULL, 
-	org_id INTEGER, 
-	patient_id INTEGER, 
-	vet_id INTEGER, 
-	status VARCHAR, 
-	start_date TIMESTAMP WITH TIME ZONE DEFAULT now(), 
-	end_date TIMESTAMP WITH TIME ZONE, 
-	notes TEXT, 
-	created_at TIMESTAMP WITH TIME ZONE DEFAULT now(), 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(org_id) REFERENCES organizations (id), 
-	FOREIGN KEY(patient_id) REFERENCES patients (id), 
-	FOREIGN KEY(vet_id) REFERENCES users (id)
-);
-
-CREATE INDEX ix_medical_attentions_vet_id ON medical_attentions (vet_id);
-
-CREATE INDEX ix_medical_attentions_org_id ON medical_attentions (org_id);
-
-CREATE INDEX ix_medical_attentions_patient_id ON medical_attentions (patient_id);
-
-CREATE INDEX ix_medical_attentions_id ON medical_attentions (id);
 
 CREATE TABLE tickets (
 	id SERIAL NOT NULL, 
@@ -235,13 +184,10 @@ CREATE TABLE tickets (
 	UNIQUE (attention_id), 
 	FOREIGN KEY(attention_id) REFERENCES medical_attentions (id), 
 	FOREIGN KEY(org_id) REFERENCES organizations (id)
-);
+)
 
-CREATE INDEX ix_tickets_ticket_number ON tickets (ticket_number);
+;
 
-CREATE INDEX ix_tickets_org_id ON tickets (org_id);
-
-CREATE INDEX ix_tickets_id ON tickets (id);
 
 CREATE TABLE ticket_items (
 	id SERIAL NOT NULL, 
@@ -252,9 +198,7 @@ CREATE TABLE ticket_items (
 	subtotal FLOAT, 
 	PRIMARY KEY (id), 
 	FOREIGN KEY(ticket_id) REFERENCES tickets (id)
-);
+)
 
-CREATE INDEX ix_ticket_items_id ON ticket_items (id);
-
-CREATE INDEX ix_ticket_items_ticket_id ON ticket_items (ticket_id);
+;
 
